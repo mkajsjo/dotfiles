cb69cd04ff1d9ff20617b6893ad7633fda90797e
Mikael Kajsjö <mkajsjo@gmail.com>
2025-05-07 13:54:39 +0200

    run dotnet on :make for F# files

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -21,3 +21,23 @@ vim.api.nvim_create_autocmd(
     "BufWritePre",
     { pattern = '*.fs', command = ':lua vim.lsp.buf.format { async = false }'}
 )
+
+-- Switch :make to 'dotnet build' for F# files
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "fsharp",
+    callback = function ()
+        if vim.fn.getcwd() == "/home/mkajsjo/dev/Captario-SUM/module-modeling" then
+            vim.opt_local.makeprg = "dotnet build --no-restore --nologo --verbosity:quiet --consoleLoggerParameters:NoSummary --consoleLoggerParameters:GenerateFullPaths-true ./src/modeling-api/modeling-api.fsproj"
+        else
+            vim.opt_local.makeprg = "dotnet build --no-restore --nologo --verbosity:quiet --consoleLoggerParameters:NoSummary --consoleLoggerParameters:GenerateFullPaths-true"
+        end
+
+        -- Set errorformat to parse dotnet build output
+        vim.opt_local.errorformat = table.concat({
+            "%f(%l\\,%c): %trror %m",   -- For error lines
+            "%f(%l\\,%c): %tarning %m", -- For warning lines
+            "%-G%.%#",                  -- Ignore any lines that don't match
+        }, ",")
+
+    end,
+})
