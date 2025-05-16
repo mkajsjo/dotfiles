73e88659d944cec26b0a847f449411030b0f7fd7
Mikael Kajsjö <mkajsjo@gmail.com>
2025-05-16 12:52:08 +0200

    refactor autocmds

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -14,9 +14,16 @@ vim.cmd [[
 vim.api.nvim_create_autocmd('BufWritePre', { callback = function() vim.lsp.buf.format() end })
 
 -- Return to last edit position when opening files
-vim.api.nvim_create_autocmd('BufReadPost', { callback = function () vim.fn.setpos('.', vim.fn.getpos('\'"'))  end })
+vim.api.nvim_create_autocmd('BufReadPost', { callback = function() vim.fn.setpos('.', vim.fn.getpos('\'"')) end })
 
-local dotnet_build = 'dotnet build --no-restore --nologo --verbosity:quiet --consoleLoggerParameters:NoSummary --consoleLoggerParameters:GenerateFullPaths-true'
+local dotnet_build = table.concat({
+    'dotnet build',
+    '--no-restore',
+    '--nologo',
+    '--verbosity:quiet',
+    '--consoleLoggerParameters:NoSummary',
+    '--consoleLoggerParameters:GenerateFullPaths-true',
+}, ' ')
 local captario_modeling_dir = '/home/mkajsjo/dev/Captario-SUM/module-modeling'
 local modeling_project = ' ./src/modeling-api/modeling-api.fsproj'
 local dotnet_build_errorformat = table.concat({
@@ -28,16 +35,16 @@ local dotnet_build_errorformat = table.concat({
 -- Switch :make to 'dotnet build' for F# files
 vim.api.nvim_create_autocmd('FileType', {
     pattern = 'fsharp',
-    callback = function ()
-        vim.opt_local.makeprg = vim.fn.getcwd() == captario_modeling_dir and dotnet_build .. modeling_project or dotnet_build
+    callback = function()
+        vim.opt_local.makeprg =
+            vim.fn.getcwd() == captario_modeling_dir
+            and dotnet_build .. modeling_project
+            or dotnet_build
         vim.opt_local.errorformat = dotnet_build_errorformat
     end,
 })
 
+-- Override treesitter @module.builtin from red to tokyonight terminal green
 vim.api.nvim_create_autocmd('BufWinEnter', {
-  pattern = '*',
-  callback = function()
-    -- Override red modules with tokyonight terminal green
-    vim.api.nvim_set_hl(0, '@module.builtin', { fg = '#73DACA' })
-  end
+    callback = function() vim.api.nvim_set_hl(0, '@module.builtin', { fg = '#73DACA' }) end
 })
